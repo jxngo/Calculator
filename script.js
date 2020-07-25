97fb1055c1c2fdadb978a050c9f2c68280d5346c
Johnny Ngo <johnnyhngo@yahoo.com>
2020-07-25 14:55:57 -0400

    skele calc design

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(operate(1,2,'+'));
+console.log(operate(1,0,'/'));
 
 
 
@@ -6,28 +6,16 @@ console.log(operate(1,2,'+'));
 function operate (num,num2,op){
     switch(op){
         case '+': 
-            return add(num,num2);
+            return num+num2;
         case '-': 
-            return sub(num,num2);
+            return num-num2;
         case '*':
-            return multiply(num,num2);
-        case '/':
-            return divide(num,num2);
-    }
-}
-
-function add (num,num2) {
-    return num+num2;
-}
-function sub (num,num2) {
-    return num-num2;
-}
-function multiply (num,num2) {
-    return num*num2;
-}
-function divide (num,num2) {
-    if(num2 === 0){
-        return "UNDEFINED";
-    }
-    return num/num2;
+            return num*num2;
+        case '/': {
+            if (num2 === 0)
+                return "UNDEFINED";
+            else 
+                return num/num2;
+        }
+   }
 }
