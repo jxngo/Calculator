ae9bd6acbafe3d500a3cb87fd6a0b81b87fdf311
Johnny Ngo <johnnyhngo@yahoo.com>
2020-08-01 20:47:12 -0400

    implementd basic 4-function calc

--- script.js
+++ script.js
@@ -1,32 +1,78 @@
 //console.log(operate(1,0,'/'));
-const y = document.getElementById("display");
-const btn = document.querySelectorAll('.calc-button, .calc-r1');
-btn.forEach((button) => {
+var op1 = null,op2 = null,operand = null;
+
+const btn = document.querySelectorAll('.calc-button, .calc-r1').forEach((button) => {
     button.addEventListener('click', () => {
-        display(button.value);
-        
-        //console.log(button.value);
+        if (button.value === '÷' || button.value === 'x' || button.value === '-' || button.value === '+'){
+            operand = button.value;
+            return;
+        }
+        if (button.value === '%' || button.value === '.') {
+            console.log("% .");
+            return;
+        }
+        if (button.value === '+/-'){
+            console.log("+/-");
+            return;
+        }
+        if (button.value === 'AC') {
+            reset();
+            return;
+        }
+
+        if (button.value === '='){
+            if(op1 === null || op2 === null || operand === null) {
+                if (op2 === null && operand !== null){
+                    op2 = op1;
+                    operate(op1,op2,operand);
+                }
+                else
+                display(op1);
+            }
+            else
+                operate(op1,op2,operand);
+            return;
+        }
+        else {
+            display(button.value);   
+            if (op1 === null)
+                op1 = button.value;
+            else 
+                op2 = button.value;
+        }
     });
 });
 
+
+
+function reset() {
+    document.getElementById("display").innerHTML = 0;
+    op1 = null;
+    op2 = null;
+    operand = null;
+}
 function display (value) {
     document.getElementById("display").innerHTML = value;
 }
-
-
 function operate (num,num2,op){
+var value = 0;
     switch(op){
         case '+': 
-            return num+num2;
+            value = (parseInt(num)+ parseInt(num2));
+            break;
         case '-': 
-            return num-num2;
-        case '*':
-            return num*num2;
-        case '/': {
-            if (num2 === 0)
-                return "UNDEFINED";
+            value = (num-num2);
+            break;
+        case 'x':
+            value = (num*num2);
+            break;
+        case '÷': {
+            if (num2 === '0') 
+                value = ("Error");
             else 
-                return num/num2;
+                value = (num/num2);
+            break; 
         }
-   }
+    }
+    display(value);
 }
